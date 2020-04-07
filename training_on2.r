957cc0772a6d8edc1849b119d7e0b5e96b00b4ee
ashleighgonza <60313543+ashleighgonza@users.noreply.github.com>
2020-04-07 18:14:41 +1000

    Character changes to code

--- training_on2.r
+++ training_on2.r
@@ -27,41 +27,42 @@ library(mlbench)
 library(caret)							
 library(randomForest)							
 library(PresenceAbsence)							
-							
-							
+														
 #Loading in and naming individual Towsey.Acoustic.csv files with corresponding pw detections							
-file1_ACI<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.ACI.csv",TRUE,",")							
-file1_BGN<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.BGN.csv",TRUE,",")							
-file1_CVR<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.CVR.csv",TRUE,",")							
-file1_DIF<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.DIF.csv",TRUE,",")							
-file1_ENT<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.ENT.csv",TRUE,",")							
-file1_EVN<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.EVN.csv",TRUE,",")							
-file1_OSC<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.OSC.csv",TRUE,",")							
-file1_PMN<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.PMN.csv",TRUE,",")							
-file1_R3D<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.R3D.csv",TRUE,",")							
-file1_RHZ<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.RHZ.csv",TRUE,",")							
-file1_RNG<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.RNG.csv",TRUE,",")							
-file1_RPS<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.RPS.csv",TRUE,",")							
-file1_RVT<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.RVT.csv",TRUE,",")							
-file1_SPT<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.SPT.csv",TRUE,",")							
-file1_SUM<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.SUM.csv",TRUE,",")							
-file1_Indices<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.Indices.csv",TRUE,",")							
-file2_ACI<-read.csv("S4A05079_20170916_181202__Towsey.Acoustic.ACI.csv",TRUE,",")							
-file2_BGN<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.BGN.csv",TRUE,",")							
-file2_CVR<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.CVR.csv",TRUE,",")							
-file2_DIF<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.DIF.csv",TRUE,",")							
-file2_ENT<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.ENT.csv",TRUE,",")							
-file2_EVN<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.EVN.csv",TRUE,",")							
-file2_OSC<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.OSC.csv",TRUE,",")							
-file2_PMN<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.PMN.csv",TRUE,",")							
-file2_R3D<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.R3D.csv",TRUE,",")							
-file2_RHZ<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.RHZ.csv",TRUE,",")							
-file2_RNG<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.RNG.csv",TRUE,",")							
-file2_RPS<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.RPS.csv",TRUE,",")							
-file2_RVT<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.RVT.csv",TRUE,",")							
-file2_SPT<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.SPT.csv",TRUE,",")							
-file2_SUM<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.SUM.csv",TRUE,",")							
-file2_Indices<-read.csv("S4A05079_20170919_181402__Towsey.Acoustic.Indices.csv",TRUE,",")							
+file1_ACI=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.ACI.csv",TRUE,",")							
+file1_BGN=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.BGN.csv",TRUE,",")							
+file1_CVR=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.CVR.csv",TRUE,",")							
+file1_DIF=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.DIF.csv",TRUE,",")							
+file1_ENT=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.ENT.csv",TRUE,",")							
+file1_EVN=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.EVN.csv",TRUE,",")							
+file1_OSC=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.OSC.csv",TRUE,",")							
+file1_PMN=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.PMN.csv",TRUE,",")							
+file1_R3D=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.R3D.csv",TRUE,",")							
+file1_RHZ=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.RHZ.csv",TRUE,",")							
+file1_RNG=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.RNG.csv",TRUE,",")							
+file1_RPS=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.RPS.csv",TRUE,",")							
+file1_RVT=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.RVT.csv",TRUE,",")							
+file1_SPT=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.SPT.csv",TRUE,",")							
+file1_SUM=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.SUM.csv",TRUE,",")							
+file1_Indices=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.Indices.csv",TRUE,",")							
+file2_ACI=read.csv("S4A05079_20170916_181202__Towsey.Acoustic.ACI.csv",TRUE,",")							
+file2_BGN=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.BGN.csv",TRUE,",")							
+file2_CVR=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.CVR.csv",TRUE,",")							
+file2_DIF=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.DIF.csv",TRUE,",")							
+file2_ENT=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.ENT.csv",TRUE,",")							
+file2_EVN=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.EVN.csv",TRUE,",")							
+file2_OSC=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.OSC.csv",TRUE,",")							
+file2_PMN=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.PMN.csv",TRUE,",")							
+file2_R3D=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.R3D.csv",TRUE,",")							
+file2_RHZ=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.RHZ.csv",TRUE,",")							
+file2_RNG=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.RNG.csv",TRUE,",")							
+file2_RPS=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.RPS.csv",TRUE,",")							
+file2_RVT=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.RVT.csv",TRUE,",")							
+file2_SPT=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.SPT.csv",TRUE,",")							
+file2_SUM=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.SUM.csv",TRUE,",")							
+file2_Indices=read.csv("S4A05079_20170919_181402__Towsey.Acoustic.Indices.csv",TRUE,",")							
+
+						
 							
 #cbinding files							
 file1_data.frameBGN=cbind(file1_ACI,file1_BGN[,-1])							
@@ -97,18 +98,18 @@ file2_data.frameCOMPLETE=cbind(file2_data.frameSUM,file2_Indices[,-1])
 							
 #rbinding acoustic indices values for all files together into one data frame							
 RBIND2=rbind(file1_data.frameCOMPLETE,file2_data.frameCOMPLETE)							
-							
+
 #load in and attach training file to total data frame							
 RBIND2_train<-read.csv("RBIND2_train.csv",FALSE,",")							
 RBIND2.FILE=cbind(RBIND2_train[,1],RBIND2)							
 RBIND2.PW=cbind(RBIND2.FILE,RBIND2_train[,3])							
-							
+
 #name file, minutes and training columns of data frame							
 names(RBIND2.PW)[1]<-paste("file_nUMBER")							
 names(RBIND2.PW)[2]<-paste("minute")							
 names(RBIND2.PW)[3869]<-paste("Plains_wanderer")							
 RBIND2.PW[,3869]							
-							
+
 #generating indices names and character strings							
 #set nscalar and nIndices							
 nscalar<-256							
@@ -196,17 +197,17 @@ my.namesIndices
 #joining character strings							
 all_names=c(my.namesACI,my.namesBGN,my.namesCVR,my.namesDIF,my.namesENT,my.namesEVN,my.namesOSC,my.namesPMN,my.namesR3D,my.namesRHZ,my.namesRNG,my.namesRPS,my.namesRVT,my.namesSPT,my.namesSUM,my.namesIndices)							
 length(all_names)							
-							
+
 #naming indices column headings							
 colnames(RBIND2.PW)[3:3868]<-paste(all_names)							
-							
+
 #balancing the ratio of positive and negative instances of target value							
 totalinstances<-nrow(RBIND2.PW)							
 totalPWinstances<-sum(RBIND2.PW[,3869])							
 idealrationumberofinstances<-totalPWinstances*2							
 totalnonPWinstances<-totalinstances-totalPWinstances							
 excessinstances<-totalnonPWinstances-totalPWinstances							
-							
+
 #balancing the ratio of positive and negative instances of target value							
 totalinstances<-nrow(RBIND2.PW)							
 totalPWinstances<-sum(RBIND2.PW[,3869])							
@@ -215,7 +216,7 @@ totalnonPWinstances<-totalinstances-totalPWinstances
 excessinstances<-totalnonPWinstances-totalPWinstances							
 whichrowsnonPW<-which(RBIND2.PW$"Plains_wanderer" == 0, arr.ind = TRUE)							
 excessremoved<-whichrowsnonPW[1:excessinstances]							
-							
+
 #view values							
 totalinstances							
 totalPWinstances							
@@ -224,23 +225,23 @@ idealrationumberofinstances
 excessinstances							
 whichrowsnonPW							
 excessremoved							
-							
+
 #remove excess instances from complete dataframe for 50:50 ratio							
 alldata50<-RBIND2.PW[-excessremoved,]							
 nrow(alldata50)  ##should equal idealrationumberofinstances for balanced dataset							
 ncol(alldata50)							
-							
+
 #Make data frame for removed instances							
 copyofexcessremoved=RBIND2.PW[excessremoved,]							
 copyofexcessremoved							
-							
+
 #shuffling data							
 set.seed(42)							
 #shuffle row indices of dataframe							
 rows<-sample(nrow(alldata50))							
 #use random vector to reorder dataset							
 shuffledalldata50<-alldata50[rows,]							
-							
+
 #Creating train and test split with file names and minutes:w							
 set.seed(100)							
 percentage_split=0.95							
@@ -249,70 +250,73 @@ training_w<- shuffledalldata50[in_training,]
 nrow(training_w) ##should ~ equal percentage_split x nrow(alldata50)							
 percentage_split*nrow(alldata50)							
 testing_split <- shuffledalldata50[-in_training,]							
-							
+
 ##is this part needed? should I just make the test set 							
 #Adding in removed non Plains_wanderer minutes (unbalanced test set)							
 testing_w<-rbind(testing_split,copyofexcessremoved)							
-							
+
 nrow(training_w)							
 nrow(testing_split)							
 nrow(shuffledalldata50)							
 nrow(copyofexcessremoved)							
-nrow(testing_w)							
+nrow(testing_w)	
+ncol(testing_w)
 ncol(training_w)							
-ncol(testing_w)							
 tail(names(training_w))							
-							
+
 #Viewing							
-training_w$"Plains_wanderer"							
-testing_w$"Plains_wanderer"							
-							
+training_w$Plains_wanderer						
+testing_w$Plains_wanderer					
+
 #Create training without file names and minutes							
 training<-training_w[,c(-1,-2)]							
-							
+
 #Create testing without file names and minutes							
 testing<-testing_w[,c(-1,-2)]							
-							
+
 #View headers							
 colnames(training)							
 colnames(testing)							
-							
+
 #Making syntactically valid names for Random Forest model 							
 make.names(training, unique = FALSE, allow_ = TRUE)							
 make.names(testing, unique = FALSE, allow_ = TRUE)							
-							
+
 #view Plains_wanderer integers before changing to factors: training and test							
 training$Plains_wanderer							
 testing$Plains_wanderer							
-							
+
 #changing Plains_wanderer integers to factors: training and test							
 training$Plains_wanderer = cut(training$Plains_wanderer,2,labels=c('No','Yes'))							
 testing$Plains_wanderer = cut(testing$Plains_wanderer,2,labels=c('No','Yes'))							
-							
+
 #view Plains_wanderer factors after changing from integers: training and test							
 training$Plains_wanderer							
 testing$Plains_wanderer							
-							
+
 #Check Plains_wanderer class:factor							
 class(training$Plains_wanderer)							
 class(testing$Plains_wanderer)							
-							
+
 #Save training and testing files							
 save(training,file="training.Rdata")							
 load(file="training.Rdata")							
 save(testing,file="testing.Rdata")							
 load(file="testing.Rdata")							
-							
+
 ==========================================================================================================							
   MACHINE LEARNING							
 ==========================================================================================================							
-  							
-  #Define the control							
-  control <- trainControl(method = "cv",							
+  
+#Define the control							
+control <- trainControl(method = "cv",							
                           number = 10,							
-                          search = "grid")							
-							
-							
+                          search = "grid")		
+
+===========================
+  DEFAULT MODEL
+===========================
+
 #Create and run model with default paramters							
 set.seed(123)							
 training_default <- train(Plains_wanderer~.,							
@@ -321,17 +325,17 @@ training_default <- train(Plains_wanderer~.,
                           metric = "Accuracy",							
                           trControl = control)							
 print(training_default)							
-							
+
 #the algorithm uses 500 trees and tested three different values of mtry: 2, 87 and 3869							
 #The final value used for the model was mtry = 2, accuracy = 0.8750000							
 #Trying to get a higher accuracy 							
-							
+
 ==========================							
   BEST MTRY							
 ==========================							
-  							
-  #search best mtry: test values 1:10							
-  set.seed(123)							
+  
+#search best mtry: test values 1:10							
+set.seed(123)							
 tuneGrid <- expand.grid(.mtry = c(1:10))							
 training_mtry<- train(Plains_wanderer~.,							
                       data = training,							
@@ -343,21 +347,21 @@ training_mtry<- train(Plains_wanderer~.,
                       nodesize = 14,							
                       ntree = 300)							
 print(training_mtry)							
-							
+
 #the final value used for the model was mtry = 4, accuracy = 0.8750000							
-							
+
 plot(training_mtry)							
-							
+
 #store the best value of mtry 							
 training_mtry$bestTune$mtry							
-							
+
 #Can store it and use it when need to tune other parameters							
 max(training_mtry$results$Accuracy)							
-							
+
 #Output 							
 best_mtry <- training_mtry$bestTune$mtry 							
 best_mtry 							
-							
+
 ==========================							
   BEST MAXNODES							
 ==========================							
